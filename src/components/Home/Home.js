e5ebc85e390e5ea3b4e4be760aeff8b02aa1f915
fsensoli <fedesensoli1997@gmail.com>
2023-10-22 14:30:51 +0200

    timer on home video

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,9 +1,9 @@
 import "./Home.css"
-import Timer from "../Timer/Timer"
 import HamburgerMenu from "../HamburgerMenu/HamburgerMenu"
 import Logo from "../Logo/Logo"
 import { ThemeContext } from "../../App"
 import { useContext } from "react"
+import { useState, useEffect } from "react"
 import homeVideo from "../../gallery/videos/homeVideo.mp4"
 import sponsor1 from "../../gallery/sponsors/UNESCOlogo.png"
 import sponsor2 from "../../gallery/sponsors/FORESTElogo.png"
@@ -18,7 +18,25 @@ import sponsor10 from "../../gallery/sponsors/UNIBOlogo.png"
 
 
 export default function Home() {
+    const [timer, setTimer] = useState([0, 0, 0, 0]);
     const darkMode = useContext(ThemeContext);
+    const timerText = "Next event starts in:"
+    const dueDate = "11/04/2023";
+
+    useEffect(() => {
+        setInterval(() => {
+            const now = new Date();
+            const partyTime = new Date(dueDate);
+            const totSeconds =  parseInt(Math.abs(partyTime - now) / 1000);
+
+            const days = Math.floor(totSeconds / (24 * 3600));
+            const hours = Math.floor((totSeconds - (days * 24 * 3600)) / 3600);
+            const minutes = Math.floor((totSeconds - (days * 24 * 3600) - (hours * 3600)) / 60);
+            const seconds = Math.floor(totSeconds - (days * 24 * 3600) - (hours * 3600) - (minutes * 60));
+
+            setTimer([days, hours, minutes, seconds]);
+        }, 1000);
+    }, [dueDate]);
 
     return (
         <>
@@ -32,7 +50,7 @@ export default function Home() {
                     Join our community
                 </div>
             </div>
-            <Timer timerText={'Next event starts in:'} dueDate={'11/04/2023'}></Timer>
+            
             {
                 /*
                 <div className="animationContainer">
@@ -41,11 +59,23 @@ export default function Home() {
                 </div>
                 */
             }
+            
             <div className="homeVideoContainer">
+                <div className="homeTimerContainer">
+                    <div className="timerTitle">{timerText}</div>
+                    <div>
+                        { String(timer[0]).padStart(2, '0') + "d " } 
+                        { String(timer[1]).padStart(2, '0') + "h "} 
+                        { String(timer[2]).padStart(2, '0') + "m "} 
+                        { String(timer[3]).padStart(2, '0') + "s "} 
+                    </div>
+                </div>
+
                 <video className="homeVideo" autoPlay muted loop>
                     <source src={homeVideo} type="video/mp4"></source>
                 </video>
-            </div>   
+            </div>
+               
 
             <div className="sponsorTitle" style={{color: darkMode ? "white" : "black"}}>
                 LOCAL PARTNERS
